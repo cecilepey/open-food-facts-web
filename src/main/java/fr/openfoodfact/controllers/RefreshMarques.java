c4bab24b6af27b8f5cea7f684682e90daeee4074
Cecile Peyras <peyrascecile@gmail.com>
2019-06-12 12:46:30 +0200

    tp 4  nouvelle servlet

--- src/main/java/fr/openfoodfact/controllers/RefreshMarques.java
+++ src/main/java/fr/openfoodfact/controllers/RefreshMarques.java
@@ -3,10 +3,12 @@ package fr.openfoodfact.controllers;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/refreshMarques/*")
 public class RefreshMarques extends HttpServlet {
 
 	@Override
